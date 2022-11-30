92eb9dde457a7704cfb9670b931052ad93455545
Andrew Hatfield <andrew8412@gmail.com>
2022-11-30 17:56:55 -0500

    react project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+<script crossorigin src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"></script>
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
