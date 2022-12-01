fb59f8e5ee1cd05ae0581dca6b7b387cdab59346
Andrew Hatfield <andrew8412@gmail.com>
2022-11-30 20:05:35 -0500

    created ui

--- src/App.js
+++ src/App.js
@@ -6,7 +6,48 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="row h-100 m-0">
+        <div className="col-2 bg-brand h-100 p-2 text-dark">
+          <div className="title mb-4"><h3>PROJECT DASHBOARD</h3></div>
+          <div className="project-dashboard">
+            <div>create project management kit</div>
 
+
+          </div>
+        </div>
+        <div className="col-10 p-0">
+          <div className="row h-100 m-0">
+            <div className="col-4 border-right">
+              <div className="title p-2 mb-4"><h3>BACKLOG</h3></div>
+              <div className="backlog">
+                <div className="card">Develop create card functionality</div>
+                <div className="card">Develop Card Moving Functionality</div>
+
+
+
+              </div>
+            </div>
+            <div className="col-4 border-right">
+              <div className="title p-2 mb-4"><h3>ACTIVE</h3></div>
+              <div className="active">
+
+
+
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="title p-2 mb-4"><h3>COMPLETED</h3></div>
+            
+              <div className="completed">
+
+                <div className='card'>Create UI</div> 
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
