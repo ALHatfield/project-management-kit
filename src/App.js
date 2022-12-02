32a2ba69a83c5c7861809ed141f78b331c3d5290
Andrew Hatfield <andrew8412@gmail.com>
2022-12-02 17:35:25 -0500

    fonts

--- src/App.js
+++ src/App.js
@@ -8,27 +8,31 @@ function App() {
     <div className="App">
       <div className="row h-100 m-0">
         <div className="col-2 bg-brand h-100 p-2 text-dark">
-          <div className="title mb-4"><h3>PROJECT DASHBOARD</h3></div>
+          <div className="title mb-4 text-center">
+            <h1 className='font-brand text-white'>Projects</h1>
+          </div>
           <div className="project-dashboard">
-            <div>create project management kit</div>
-
+            <div className='project-title selected'>Create Project Management Kit</div>
+            <div className="project-title">Another Project</div>
 
           </div>
         </div>
         <div className="col-10 p-0">
           <div className="row h-100 m-0">
             <div className="col-4 border-right">
-              <div className="title p-2 mb-4"><h3>BACKLOG</h3></div>
+              <div className="title p-2 mt-2 mb-4"><h3>BACKLOG</h3></div>
               <div className="backlog">
                 <div className="card">Develop create card functionality</div>
-                <div className="card">Develop Card Moving Functionality</div>
+                <div className="card">Develop card moving functionality</div>
+                <div className="card">Project selection funciontality</div>
+                <div className="card">Persistent Data</div>
 
 
 
               </div>
             </div>
             <div className="col-4 border-right">
-              <div className="title p-2 mb-4"><h3>ACTIVE</h3></div>
+              <div className="title p-2 mt-2 mb-4"><h3>ACTIVE</h3></div>
               <div className="active">
 
 
@@ -36,10 +40,8 @@ function App() {
               </div>
             </div>
             <div className="col-4">
-              <div className="title p-2 mb-4"><h3>COMPLETED</h3></div>
-            
+              <div className="title p-2 mt-2 mb-4"><h3>COMPLETED</h3></div>
               <div className="completed">
-
                 <div className='card'>Create UI</div> 
 
 
